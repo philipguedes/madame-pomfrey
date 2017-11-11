6ef9aad725bd947dbd347cef9c5ccbf8e85f0bb0
Philip Guedes <philipguedes@gmail.com>
2017-11-11 19:23:38 -0200

    env as port

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 let http = require('http')
-
 let app = require('./config/express')
 
 
-http.createServer(app).listen('80', function(res) {
-  console.log('done at port 4242')
+var port = process.env.PORT || 4200;
+
+http.createServer(app).listen(port, function(res) {
+  console.log('done at port ' + port);
 });
