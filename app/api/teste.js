9c446794c80c58fed6e083ce6cf7ce340b01a1f0
Philip Guedes <philipguedes@gmail.com>
2017-11-11 20:29:46 -0200

    Add examples

--- app/api/teste.js
+++ app/api/teste.js
@@ -1,3 +1,9 @@
+'use-strict'
+
+var firebase = require('../../config/database')
+var database = firebase.database()
+
+
 var api = {};
 
 api.teste = function (req, res) {
@@ -7,9 +13,18 @@ api.teste = function (req, res) {
   res.json({
     mensagem: 'sucesso'
   });
-  
+
 }
 
+api.testeDados = function (req, res) {
+  database.ref('"99999999999"').once('value').then( (snap) => {
+    res.status(200);
+    res.json({
+      mensagem: snap
+    })
+  })
+
+}
 
 
 module.exports = api;
--- app/routes/teste.js
+++ app/routes/teste.js
@@ -2,4 +2,8 @@ module.exports = function(app) {
   var api = app.api.teste;
 
   app.route('/teste').get(api.teste);
+
+  app.route('/teste/dados').get(api.testeDados);
 }
+
+
