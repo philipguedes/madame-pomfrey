c6fee73d6fbe4dc4bb59a7a81835d0885053a594
Lukas Camargo (Americas) <lukas.camargo@dimensiondata.com>
2017-11-12 09:41:24 -0200

    haha

--- public/js/controllers/certificado-controller.js
+++ public/js/controllers/certificado-controller.js
@@ -1,7 +1,8 @@
 angular
     .module('madame-pomfrey')
-    .controller('CertificadoController', function ($scope, $http) {
+    .controller('CertificadoController', function ($scope, $http, $location, $anchorScroll) {
         vm = this;
+        vm.buscar = { certificado: 'c511cf33-3d17-48df-9c9c-25397a6ce012', path: '706905129075639' };
 
         vm.buscarCertificado = function () {
             $http.post('/user/certificate/recover', vm.buscar)
@@ -10,6 +11,9 @@ angular
                         console.log(erro);
                     } else {
                         console.log(resposta);
+                        vm.usuario = resposta.data.response;
+                        $location.hash('conteudo');
+                        $anchorScroll();
                     }
                 })
         }
